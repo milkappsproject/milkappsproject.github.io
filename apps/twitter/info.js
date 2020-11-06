e30f6ed27e768ce2b10216079bf3858dea1a33ee
milkappsproject <73395488+milkappsproject@users.noreply.github.com>
2020-11-06 21:50:57 +0300

    Added some apps

---
+++ apps/twitter/info.js
@@ -0,0 +1,42 @@
+var appi = {
+	"id": "twitter",
+	"dev": "Twitter (?)",
+	"link": "https://www.twitter.com",
+	"name": "Twitter",
+	"description": "Twitter is a 'microblogging' system that allows you to send and receive short posts called tweets. Tweets can be up to 140 characters long and can include links to relevant websites and resources. (UKRI)",
+	"reward": 5
+}
+
+
+
+//////////////////////////////////////////////////////////
+
+
+
+var installC = function(){
+	if(JSON.parse(Cookies.get("apps")).includes(appi.id)) return;
+	if(appi.reward < 0 && -(appi.reward) > Number(Cookies.get("balance"))){
+		alert("Not enough points! You can get points by installing free apps.");
+		return;
+	}
+	Cookies.set("apps", JSON.stringify(JSON.parse(Cookies.get("apps")).concat([appi.id])));
+	Cookies.set(appi.id, JSON.stringify(appi));
+	setTimeout(function(){
+		new Notification("MilkApps", {"icon": "icon.png", "body": appi.name + " successfully installed! Click here to open."}).onclick = tryC;
+	}, 0);
+	Cookies.set("actions", JSON.stringify(JSON.parse(Cookies.get("actions")).concat(["Installed " + appi.name])));
+	if(JSON.parse(Cookies.get("gotbonus")).includes(appi.id)) return;
+	Cookies.set("balance", Number(Cookies.get("balance")) + appi.reward);
+	Cookies.set("gotbonus", JSON.stringify(JSON.parse(Cookies.get("gotbonus")).concat([appi.id])));
+	playsound("/resources/audio/install_custom.wav");
+}
+var tryC = function(){
+	window.open(appi.link, "", "width=800,height=600,top=100,left=100,menubar=no,toolbar=no,location=no,status=no");
+	playsound("/resources/audio/try.wav");
+}
+$("#name").text(appi.name);
+$("#dev").text("by " + appi.dev);
+$("#description").text(appi.description);
+if(appi.reward < 0) $("#reward").text("You'll spend " + -appi.reward + " points");
+else $("#reward").text("You'll gain " + appi.reward + " points");
+$("#id").text("ID: " + appi.id);
